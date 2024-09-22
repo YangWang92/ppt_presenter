fefc8b118c7f5201121b1680a1b629b236f9c6dc
Yang Wang <wyatuestc@gmail.com>
2024-09-22 21:21:00 +0800

    add retry

--- ppt_presenter.py
+++ ppt_presenter.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+import time
 import os
 import tempfile
 import argparse
@@ -9,15 +10,17 @@ from subprocess import call
 
 from pdf2image import convert_from_path
 from pptx import Presentation
-from gtts import gTTS
+from gtts import gTTS, gTTSError
+
+__author__ = ['chaonan99', 'yangwang92']
 
 
-__author__ = ['chaonan99']
+# Sometimes ffmpeg is avconv
+FFMPEG_NAME = 'ffmpeg'
+# FFMPEG_NAME = 'avconv'
 
-
-## Sometimes ffmpeg is avconv
-# FFMPEG_NAME = 'ffmpeg'
-FFMPEG_NAME = 'avconv'
+max_retries = 1000
+retry_delay = 5
 
 
 def ppt_presenter(pptx_path, pdf_path, output_path):
@@ -33,11 +36,20 @@ def ppt_presenter(pptx_path, pdf_path, output_path):
                 audio_path = os.path.join(temp_path, 'frame_{}.mp3'.format(i))
 
                 image.save(image_path)
-                tts.save(audio_path)
+
+                # retry to avoid error
+                for retry in range(max_retries):
+                    try:
+                        tts.save(audio_path)
+                        break
+                    except Exception as e:
+                        print(f"Retry {retry + 1}/{max_retries} failed: {e}")
+                        time.sleep(retry_delay)
 
                 ffmpeg_call(image_path, audio_path, temp_path, i)
+            # TODO handle the case where slide has no notes
 
-        video_list = [os.path.join(temp_path, 'frame_{}.ts'.format(i)) \
+        video_list = [os.path.join(temp_path, 'frame_{}.ts'.format(i))
                       for i in range(len(images_from_path))]
         video_list_str = 'concat:' + '|'.join(video_list)
         ffmpeg_concat(video_list_str, output_path)
@@ -47,8 +59,10 @@ def ffmpeg_call(image_path, audio_path, temp_path, i):
     out_path_mp4 = os.path.join(temp_path, 'frame_{}.mp4'.format(i))
     out_path_ts = os.path.join(temp_path, 'frame_{}.ts'.format(i))
     call([FFMPEG_NAME, '-loop', '1', '-y', '-i', image_path, '-i', audio_path,
-          '-c:v', 'libx264', '-tune', 'stillimage', '-c:a', 'libfdk_aac',
-          '-b:a', '192k', '-pix_fmt', 'yuv420p', '-shortest', out_path_mp4])
+          # Ensures width and height divisible by 2
+          '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',
+          '-c:v', 'libx264', '-tune', 'stillimage', '-c:a', 'aac',
+          '-b:a', '128k', '-pix_fmt', 'yuv420p', '-shortest', out_path_mp4])
     call([FFMPEG_NAME, '-y', '-i', out_path_mp4, '-c', 'copy',
           '-bsf:v', 'h264_mp4toannexb', '-f', 'mpegts', out_path_ts])
 
